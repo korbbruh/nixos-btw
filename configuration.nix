938c050f0330cea22a42e98cc076b55a8ed1fbdb
korv <laceras.korb@gmail.com>
2026-08-06 20:05:28 +0800

    polkit agent?

--- configuration.nix
+++ configuration.nix
@@ -146,27 +146,6 @@
     nerd-fonts.jetbrains-mono
   ];
 
-  security.polkit.enable = true;
-
-  systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-  };
-   extraConfig = ''
-     DefaultTimeoutStopSec=10s
-   '';
-};
-
   #### Do not change ###########################################
 
   system.stateVersion = "26.05";
