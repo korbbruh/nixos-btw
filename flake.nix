3b603c5019203a8d1c35c08c0b2611a447485b01
korv <laceras.korb@gmail.com>
2026-08-06 19:17:41 +0800

    initial

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "MangoWM on NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    mangowm = {
+      url = "github:mangowm/mango";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          inputs.nixos-hardware.nixosModules.asus-zephyrus-ga503
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
