4c519f3c089d1070db86d78883a41c848c40cf7d
korv <laceras.korb@gmail.com>
2026-08-08 23:41:32 +0800

    some stuffs 2

--- configuration.nix
+++ configuration.nix
@@ -147,18 +147,18 @@
   ];
 
 security.polkit.enable = true;
-
-systemd.user.services.polkit-gnome-authentication-agent-1 = {
-  description = "polkit-gnome-authentication-agent-1";
+services.asusd.enable = true;
+services.power-profiles-daemon.enable = true;
+systemd.user.services.polkit-kde-agent = {
+  description = "polkit-kde-authentication-agent-1";
   wantedBy = [ "graphical-session.target" ];
   wants = [ "graphical-session.target" ];
   after = [ "graphical-session.target" ];
   serviceConfig = {
     Type = "simple";
-    ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
     Restart = "on-failure";
     RestartSec = 1;
-    TimeoutStopSec = 10;
   };
 };
 
