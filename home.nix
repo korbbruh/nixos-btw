592f965061e120963fdd9ef356e599f92f5450af
korv <laceras.korb@gmail.com>
2026-08-11 22:32:15 +0800

    second home updoot

--- home.nix
+++ home.nix
@@ -5,11 +5,52 @@
   home.stateVersion = "26.05";
   programs.home-manager.enable = true;
 
+  programs.starship.enable = true;
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+    options = [ "--cmd" "cd" ];
+  };
+  programs.direnv = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
   programs.fish = {
     enable = true;
+
     shellAliases = {
       ls = "eza --icons --group-directories-first -1";
-      neofetch = "fastfetch -c /examples/13";
+    };
+
+    shellAbbrs = {
+      lg = "lazygit";
+      gd = "git diff";
+      ga = "git add .";
+      gc = "git commit -am";
+      gl = "git log";
+      gs = "git status";
+      gst = "git stash";
+      gsp = "git stash pop";
+      gp = "git push";
+      gpl = "git pull";
+      gsw = "git switch";
+      gsm = "git switch main";
+      gb = "git branch";
+      gbd = "git branch -d";
+      gco = "git checkout";
+      gsh = "git show";
+      l = "ls";
+      ll = "ls -l";
+      la = "ls -a";
+      lla = "ls -la";
+    };
+
+    functions = {
+      mark_prompt_start = {
+        onEvent = "fish_prompt";
+        body = ''echo -en "\e]133;A\e\\"'';
+      };
     };
   };
 }
