64824c1dde102141d1556cb619a5de8c3602dbce
korv <laceras.korb@gmail.com>
2026-08-11 22:21:09 +0800

    first updoot

--- home.nix
+++ home.nix
@@ -4,4 +4,12 @@
   home.homeDirectory = "/home/kl";
   home.stateVersion = "26.05";
   programs.home-manager.enable = true;
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "eza --icons --group-directories-first -1";
+      neofetch = "fastfetch -c /examples/13";
+    };
+  };
 }
