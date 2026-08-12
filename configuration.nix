dd9f0713b37a5765379af14fd801190ac655ccfd
korv <laceras.korb@gmail.com>
2026-08-12 22:04:49 +0800

    cursors + thunar again + dolphin

--- configuration.nix
+++ configuration.nix
@@ -157,6 +157,7 @@ security.pam.services.swaylock = { };
     thunar-archive-plugin
     thunar-vcs-plugin
     thunar-volman
+    dolphin
     kdePackages.polkit-kde-agent-1
     cliphist
     wl-clipboard
