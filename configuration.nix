a585e8652451ff0cbe18b0321b10e563ce0e724c
korv <laceras.korb@gmail.com>
2026-08-20 06:42:19 +0800

    gtk and qt stuff for polkit auth

--- configuration.nix
+++ configuration.nix
@@ -261,19 +261,6 @@ systemd.user.services.xwayland-satellite = {
 
 systemd.user.services.polkit-kde-agent = {
   description = "polkit-kde-authentication-agent-1";
-  wantedBy = [ "graphical-session.target" ];
-  wants = [ "graphical-session.target" ];
-  after = [ "graphical-session.target" ];
-  serviceConfig = {
-    Type = "simple";
-    ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-    Restart = "on-failure";
-    RestartSec = 1;
-  };
-};
-
-systemd.user.services.polkit-kde-agent = {
-  description = "polkit-kde-authentication-agent-1";
   after = [ "graphical-session.target" ];
   environment = {
     QT_QPA_PLATFORMTHEME = "gnome";
@@ -281,7 +268,7 @@ systemd.user.services.polkit-kde-agent = {
   serviceConfig = {
     Type = "simple";
     ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-    Restart = "always";
+    Restart = "on-failure";
     RestartSec = 1;
   };
 };
