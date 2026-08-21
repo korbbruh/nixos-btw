b7dbdede55484c393c84e731d39157e4d1a607c2
korv <laceras.korb@gmail.com>
2026-08-21 11:23:54 +0800

    flake update

--- configuration.nix
+++ configuration.nix
@@ -222,7 +222,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 14d";
+    options = "--delete-older-than 7d";
   };
 
   # ==========================================================================
