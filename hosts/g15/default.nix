ccccc926eee719c0edb4c0cbb8ca38a07d86e292
korbbruh <laceras.korb@gmail.com>
2026-09-07 21:22:19 +0800

    flake update

--- hosts/g15/default.nix
+++ hosts/g15/default.nix
@@ -59,9 +59,9 @@
   # Networking
   # ==========================================================================
 
-  #networking.networkmanager.enable = true;
-  #networking.wireless.iwd.enable = true;
   networking.networkmanager.enable = true;
+ # networking.wireless.iwd.enable = true;
+  #networking.networkmanager.enable = true;
   #  wifi.backend = "iwd";
   #};
 
